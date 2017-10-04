eed7ed877410dc83df7ca779a201c8fb05e04a09
iaw39443990 <iaw39443990@h09.informatica.escoladeltreball.org>
2017-10-04 17:01:49 +0200

    Main class addedd

---
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.escoladeltreball.collectionsdemo1;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author iaw39443990
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		List<String> list = new ArrayList();
+		
+		list.add("Alpha");
+		list.add("Charlie");
+		list.add("Bravo");
+		list.add("Echo");
+		list.add("Delta");
+		list.add("Zulu");
+		
+		//Eliminamos elemento de la lista
+		list.remove("Charlie");
+		
+		//Ordenamos lista
+		Collections.sort(list);
+		
+		
+		//Mostramos lista
+		for(String string: list) {
+			System.out.println(string);
+		}
+		
+		
+	}
+
+}
